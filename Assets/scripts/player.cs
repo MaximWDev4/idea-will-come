56ce60a74b8e84d7e9e410f5a77c6a0107019afd
J_max <58228107+MaximWDev4@users.noreply.github.com>
2020-05-13 20:19:41 +0600

    Player Configure
    Player can jump, move, collect coins, die(

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour
+{
+    public GameObject playerCapsule;
+    
+    public Text score;
+    public int coins = 0;
+    void OnTriggerEnter2D(Collider2D col) {
+
+        //obstacles
+        if (col.gameObject.name == "obstacles" ){
+            Die();
+        }
+        //coin
+        if (col.gameObject.name == "coin"){
+            coins++;
+            Debug.Log(coins);
+            Destroy(col.gameObject);
+            score.text = ( "Score: " + coins);
+        }
+        
+    }
+    
+    void Die()
+    {
+        Destroy(playerCapsule);
+    }
+}
