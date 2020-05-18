39892ff8e9c387cbf5816ded6970e70318712a0a
J_max <58228107+MaximWDev4@users.noreply.github.com>
2020-05-18 23:20:59 +0600

    New scene

---
+++ Assets/scripts/ButtonCreator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCreator : MonoBehaviour
+{
+    public Canvas canvas;
+    public Font newFont;
+   
+   void Start(){
+       CreateButton();
+   }
+    void CreateButton(){
+        GameObject newButton = new GameObject("New Button", typeof(Image), typeof(Button));
+        newButton.transform.SetParent(canvas.transform);
+        GameObject newText = new GameObject("New Text", typeof(Text));
+        newText.transform.SetParent(newButton.transform);
+        newText.GetComponent<Text>().text = "New Button";
+        newText.GetComponent<Text>().font = newFont;
+        RectTransform rt = newText.GetComponent<RectTransform>();
+        rt.anchorMin = new Vector2(0, 0);
+        rt.anchorMax = new Vector2(1, 1);
+        rt.anchoredPosition = new Vector2(0, 0);
+        rt.sizeDelta = new Vector2(0, 0);
+        rt.position = new Vector3(0, -90, 0);
+        newText.GetComponent<Text>().color = new Color(0, 0, 0);
+        newText.GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
+        newButton.GetComponent<Button>().onClick.AddListener(delegate { restart(); });
+    }
+
+    void restart(){
+      Application.LoadLevel (Application.loadedLevel);
+    }
+}
