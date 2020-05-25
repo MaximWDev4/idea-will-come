7bf170b72c2c30cae6c2c544e514c8578d3ea72e
J_max <58228107+MaximWDev4@users.noreply.github.com>
2020-05-25 19:08:26 +0600

    new level

---
+++ Assets/scripts/GraphicsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class GraphicsController : MonoBehaviour
+{
+    public Dropdown dropdown; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropdown.ClearOptions();
+        dropdown.AddOptions(QualitySettings.names.ToList());
+        dropdown.value = QualitySettings.GetQualityLevel();
+    }
+
+    // Update is called once per frame
+    public void ChangeQuality(){
+        QualitySettings.SetQualityLevel(dropdown.value);
+    }
+}
